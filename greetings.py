39b7b069df41309cbb5a1bf62f2c6c7ea4bbbc8b
mkaoy2k <47133637+mkaoy2k@users.noreply.github.com>
2022-02-17 18:57:13 -0800

    Update greetings.py
    3 functions defined

--- greetings.py
+++ greetings.py
@@ -1,25 +1,54 @@
-""" 呼叫輸入函式 input(<提示字串>)，
-Python 能印出一段提示字串，
-然後等使用者輸入資訊，
-按 enter 鍵後，
-Python 再返回程式生成字串物件。
-"""
-greeting = '哈囉'
+# 打招呼函式
 
-# 從鍵盤輸入，呼叫 input(<提示字串>)函式
-name = input("請問你的名字叫: ")
-if name == '':
-    # 空白的話，用預設值
-    name = '尤勇'   
+def input_name(name=None):
+    """ 呼叫輸入函式 input(<提示字串>)，
+    Python 能印出一段提示字串，
+    然後等使用者輸入資訊，
+    按 enter 鍵後，
+    返回名字字串。
 
-# 螢幕上輸出
-# 用法1：字串物件的 'format' 方法
-print("用字串物件的 'format' 方法...")
-message1 = '{}, {}. 歡迎!'.format(greeting, name)
-print(message1)
-print()
+    用法：
+    input_name('<你的名字>')
 
-# 用法2： Python 版本3以後才有的 'f-字串指令')
-print("用 Python 版本3以後才有的 'f-字串指令'...")
-message2 = f'{greeting}, {name}. 歡迎!'
-print(message2)
+    1. <你的名字>忽略時，會要求輸入。
+    2. 輸入空白時，預設值是'尤勇'
+    """
+
+    if name == None:
+        # <你的名字>忽略時，呼叫 input(<提示字串>)函式
+        name = input("請問你的名字叫: ")
+        if name == '':
+            name = '尤勇'   # 輸入空白時，用預設值
+    return name
+
+
+def print_hello1(name,
+                 greeting='哈囉',
+                 welcome='歡迎'
+                 ):
+    """
+    以字串物件的 'format' 方法列印
+
+    用法：
+    print_hello1(greeting, name)
+    """
+    message1 = '{}, {}. {}!'.format(
+        greeting,
+        name,
+        welcome)
+    print(message1)
+
+
+def print_hello2(name,
+                 greeting='哈囉',
+                 welcome='歡迎'
+                 ):
+    """
+    Python 版本3以後才有的
+    以'f-字串指令'列印
+
+    用法：
+    print_hello2(greeting, name)
+    """
+    message2 = f'{greeting}, {name}. {welcome}!'
+    print(message2)
