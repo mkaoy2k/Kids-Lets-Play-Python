39d53781d9274d92d9cb9b86628f36163d91e64f
Michael Kao <mkaoy2k@gmail.com>
2023-04-22 16:06:07 -0700

    Added functional tests in main()

--- greetings.py
+++ greetings.py
@@ -2,13 +2,10 @@
 
 def input_name(name=None):
     """ 呼叫輸入函式 input(<提示字串>)，
-    Python 能印出一段提示字串，
-    然後等使用者輸入資訊，
-    按 enter 鍵後，
-    返回名字字串。
+    Python 能印出一段提示字串，然後等使用者輸入資訊，按 enter 鍵後，返回名字字串。
 
     用法：
-    input_name('<你的名字>')
+        input_name('<你的名字>')
 
     1. <你的名字>忽略時，會要求輸入。
     2. 輸入空白時，預設值是'尤勇'
@@ -17,6 +14,7 @@ def input_name(name=None):
     if name == None:
         # <你的名字>忽略時，呼叫 input(<提示字串>)函式
         name = input("請問你的名字叫: ")
+    else:
         if name == '':
             name = '尤勇'   # 輸入空白時，用預設值
     return name
@@ -30,7 +28,7 @@ def print_hello1(name,
     以字串物件的 'format' 方法列印
 
     用法：
-    print_hello1(greeting, name)
+        print_hello1(greeting, name)
     """
     message1 = '{}, {}. {}!'.format(
         greeting,
@@ -44,11 +42,18 @@ def print_hello2(name,
                  welcome='歡迎'
                  ):
     """
-    Python 版本3以後才有的
-    以'f-字串指令'列印
+    Python 版本3以後才有的以'f-字串指令'列印
 
     用法：
-    print_hello2(greeting, name)
+        print_hello2(greeting, name)
     """
     message2 = f'{greeting}, {name}. {welcome}!'
     print(message2)
+
+if __name__ == '__main__':
+    print(input_name())
+    print(input_name(name=''))
+    print(input_name(name='Michael'))
+
+    print_hello1(name='Michael 1')
+    print_hello2(name='Michael 2')
